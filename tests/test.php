55a425a87674d39bbd1388350613f5285cf23ff2
James <james@sglms.com>
2021-12-10 13:46:26 -0300

    Housekeeping

--- tests/test.php
+++ tests/test.php
@@ -4,12 +4,12 @@ require_once '../vendor/autoload.php'; // Autoload files using Composer autoload
 
 use JamesRCZ\PdfDownloader;
 
-/* $url = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"; */
+$url = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
 
 $url = "http://www.sernapesca.cl/sites/default/files/res.ex_.3120-2021.pdf";
 $pdfDownloader = new PdfDownloader($url, "/tmp/", "pdf-downloader.pdf", true);
-$pdfDownloader->download();
+echo $pdfDownloader->download() ? _("Pdf downloaded") : _("Error");
 
 $url = "https://www.w3.org/WAI/ER/";
 $pdfDownloader = new PdfDownloader($url, "/tmp/", "pdf-downloader-pdf-generator.pdf", true);
-$pdfDownloader->printPdf();
+echo $pdfDownloader->printPdf() ? _("Pdf generated") : _("Error");
